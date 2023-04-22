e28df41f47cce7978b4022ab103179beb440b361
Saurabh Raj <72649949+5aura6h@users.noreply.github.com>
2023-04-22 12:13:41 +0530

    Add files via upload

---
+++ Scientific Calculator/MainClass.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+
+    public class MainClass {
+        public static void main(String[] args) {
+
+            ScientificCalculator scientificCalculator = new ScientificCalculator();
+
+            scientificCalculator.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            scientificCalculator.setSize(400, 350);
+            scientificCalculator.setVisible(true);
+
+
+        }
+    }
